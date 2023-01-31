fef6124639a5dd9f92736c816e711027aaca70f7
Jeongin Lee <jil8885@hanyang.ac.kr>
2023-01-31 16:54:27 +0900

    feat: Master 노드의 데이터베이스 접속할 수 있게 수정

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 import asyncio
 
 from sqlalchemy import delete
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import sessionmaker
 
 from models import ShuttleRoute, ShuttleRouteStop, ShuttleTimetable
 from scripts.route import get_route_list
 from scripts.route_stop import get_route_stop_list
 from scripts.timetable import insert_shuttle_timetable
-from utils.database import get_db_engine
+from utils.database import get_db_engine, get_master_db_engine
 
 
 async def main():
@@ -16,6 +17,16 @@ async def main():
     session = session_constructor()
     if session is None:
         raise RuntimeError("Failed to get db session")
+    try:
+        await execute_script(session)
+    except OperationalError:
+        connection = get_master_db_engine()
+        session_constructor = sessionmaker(bind=connection)
+        session = session_constructor()
+        await execute_script(session)
+
+        
+async def execute_script(session):
     session.execute(delete(ShuttleTimetable))
     session.execute(delete(ShuttleRouteStop))
     session.execute(delete(ShuttleRoute))
